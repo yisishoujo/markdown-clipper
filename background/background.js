a9a5eb1782a74cf29c01ac29f1dd4103d4a6521e
yisishoujo <52815515+yisishoujo@users.noreply.github.com>
2024-09-18 18:23:58 +0800

    Update background.js
    yaml frontmatter

--- background/background.js
+++ background/background.js
@@ -10,17 +10,17 @@ function createReadableVersion(dom) {
   return article;
 }
 
-function convertArticleToMarkdown(article) {
+function convertArticleToMarkdown(article, formattedDate, sourceUrl) {
   var turndownService = new TurndownService()
   var markdown = turndownService.turndown(article.content);
   
   //add article titel as header
-  markdown = "# " + article.title + "\n" + markdown;
+  markdown = "---\ntitle: \"" + article.title + "\"\ndate: " + formattedDate + "\ntags: [\"\"]\n---\n\n[source](" + sourceUrl + ")\n\n" + markdown;
 
   //add summary if exist
-  if (article.excerpt != null) {
-    markdown = "> " + article.excerpt + "\n\n" + markdown;
-  }
+  /* if (article.excerpt != null) {
+   *   markdown = "> " + article.excerpt + "\n\n" + markdown;
+   * } */
   return markdown;
 }
 
@@ -74,7 +74,14 @@ function downloadMarkdown(markdown, article) {
 
 
 //function that handles messages from the injected script into the site
-function notify(message) {
+function notify(message, sender) {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day}`;
+  
   var parser = new DOMParser();
   var dom = parser.parseFromString(message.dom, "text/html");
   if (dom.documentElement.nodeName == "parsererror"){
@@ -82,6 +89,7 @@ function notify(message) {
   } 
 
   var article = createReadableVersion(dom);
-  var markdown = convertArticleToMarkdown(article);
+  var sourceUrl = sender.tab.url;
+  var markdown = convertArticleToMarkdown(article, formattedDate, sourceUrl);
   downloadMarkdown(markdown, article);
 }
